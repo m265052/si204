59cd815225175bc9e99713a3b718ab191cdd22b5
m265052 <m265052@usna.edu>
2024-02-11 20:17:26 -0500

    msg

--- proj01/green.cpp
+++ proj01/green.cpp
@@ -14,28 +14,34 @@
         cin >> namef;
         cout << "Background file: ";
         cin >> nameb;
-        ifstream fin(namef);
-        ifstream fin2(nameb);
+        ifstream fin(namef);        //foreground fin
+        ifstream fin2(nameb);       //background fin2
         //print to an output file
         cout << "Output file: ";
         cin >> nameo;
         ofstream fout(nameo);
 
-        //ensure the files exists
+        //ensure the files exist
         if(!fin || !fin2)
         {
             cout << "Error: Input file not found" << endl;
             return 1;
         }
 
-        //prepare to read the file
-        string header;
-        int width, height, maxValue, r, g, b, gray;
+        //prepare to read the files
+        string header, header2;
+        int width, height, maxValue, r, g, b, r2, g2, b2, width2, height2, maxValue2;
 
-        //take in the descriptive values of the file
+        //take in the descriptive values of the files and confirm they match
         fin >> header >> width >> height >> maxValue;
+        fin2 >> header2 >> width2 >> height2 >> maxValue2;
+        if( width != width2 || height != height2)
+        {
+            cout << "Error: Images have different sizes" << endl;
+            return 1;
+        }
         //put these back in place in new ppm
-        fout << header << endl << width << " " << height << endl << maxValue << endl;
+        else fout << header << endl << width << ' ' << height << endl << maxValue << endl;
 
         //for loop to read the rows
         for(int i=0; i < height; i++)
@@ -44,11 +50,14 @@
             for(int j=0; j < width; j++)
             {
             //take in and read out the red, green, and blue values
+            //check if greenscreen and if so, fout the background pixels
             fin >> r >> g >> b;
-            gray = (r + b + g) / 3.0;
-            fout << gray << " " << gray << " " << gray << " ";
+            fin2 >> r2 >> g2 >> b2;
+            if(r == 0 && g == 255 && b == 0)
+            fout << r2 << ' ' << g2 << ' ' << b2 << ' ';
+            else fout << r << ' ' << g << ' ' << b << ' ';
             }
         }
         cout << "Image saved to " << nameo << endl;
         return 0;
-    }
+    } 
---
+++ proj01/rowshift.cpp
@@ -0,0 +1,78 @@
+    //Will Pitkin       m265052
+    //Proj01 Pt4        11Feb24
+    //this part will implement the green screen with a row shifted image
+
+    #include <iostream>
+    #include <fstream>
+    using namespace std;
+
+    int main()
+    {
+        //ask for the foreground file, then the background
+        cout << "Foreground file: ";
+        string namef, nameb, nameo;     //name: foreground, background, output
+        cin >> namef;
+        cout << "Background file: ";
+        cin >> nameb;
+        ifstream fin(namef);        //foreground fin
+        ifstream fin2(nameb);       //background fin2
+        int rs;                     //row shift
+        cout << "Row shift: ";
+        cin >> rs;
+        //print to an output file
+        cout << "Output file: ";
+        cin >> nameo;
+        ofstream fout(nameo);
+
+        //ensure the files exist
+        if(!fin || !fin2)
+        {
+            cout << "Error: Input file not found" << endl;
+            return 1;
+        }
+
+        //prepare to read the files
+        string headerfg, headerbg;
+        int widthfg, heightfg, maxValuefg, rfg, gfg, bfg; 
+        int rbg, gbg, bbg, widthbg, heightbg, maxValuebg;
+
+        //take in the descriptive values of the files
+        fin >> headerfg >> widthfg >> heightfg >> maxValuefg;
+        fin2 >> headerbg >> widthbg >> heightbg >> maxValuebg;
+
+        //determine if the row shift is allowed
+        if(heightfg + rs > heightbg)
+        {
+            cout << "Error: The foreground goes past the background" << endl;
+            return 1;
+        }
+
+        //fout the size of the background image
+        fout << headerbg << endl << widthbg << ' ' 
+             << heightbg << endl << maxValuebg << endl;
+
+        //for loop to read the rows
+        for(int i=0; i < heightbg; i++)
+        {
+            //nested for loop to analyze each row
+            for(int j=0; j < widthbg; j++)
+            {
+                //still take in the background image
+                fin2 >> rbg >> gbg >> bbg;
+                //determine where the row shift starts and ends
+                //output the green screen effect in that area
+                //the following parameters make the following statement only
+                //happen if the row is within the rowshift
+                if(i >= rs && i <= rs + heightfg && j < widthfg)
+                {
+                    fin >> rfg >> gfg >> bfg;
+                    if(rfg == 0 && gfg == 255 && bfg == 0)
+                    fout << rbg << ' ' << gbg << ' ' << bbg << ' ';
+                    else fout << rfg << ' ' << gfg << ' ' << bfg << ' ';
+                }
+                else fout << rbg << ' ' << gbg << ' ' << bbg << ' ';
+            }
+        }
+        cout << "Image saved to " << nameo << endl;
+        return 0;
+    } 
